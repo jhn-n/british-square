4ecceea0159fa93228bbd229f4b7f51fc612d437
jhn-n <182117243+jhn-n@users.noreply.github.com>
2025-05-31 16:38:45 +0100

    Add linear sequence bit function and refactor

--- britishBB2.js
+++ britishBB2.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 
 // need WIDTH * HEIGHT <= 32 due to bitboard max size
 // need WIDTH * HEIGHT <= 25 or hashmap encoding may not work
@@ -18,43 +18,49 @@ let oppBB = 0;
 let blueToMove = true;
 
 const movesPlayed = [];
-movesPlayed.forEach(m => makeMove(m));
+movesPlayed.forEach((m) => makeMove(m));
 
 console.time("Analysis");
 analyseBoard();
 console.timeEnd("Analysis");
-console.log("Hashmap size:", hashMap.size, (mapSize(hashMap) / 2 ** 20).toFixed(2), "MB")
-
+console.log(
+    `Hashmap size: ${hashMap.size} (${(mapSize(hashMap) / 2 ** 20).toFixed(2)}MB)`
+);
 
 function analysePosition(pBB, oBB, blue) {
     let best = -100;
-    const moveSpaceBB = validMoves(pBB, oBB);
+    const moveSpace = validMoves(pBB, oBB);
+    if (moveSpace === 0) {
+        return scoreAfterPass(pBB, oBB, blue);
+    }
+
     for (let m = 0; m < numSquares; m++) {
-        if (getBit(moveSpaceBB, m)) {
+        if (getBit(moveSpace, m)) {
             const score = -analyse(oBB, setBit(pBB, m), !blue);
-            best = (score > best) ? score : best;
+            best = score > best ? score : best;
         }
     }
-    return (best === -100) ? scoreAfterPass(pBB, oBB, blue) : best;
+    return best;
 }
 
 function analysePositionHash(pBB, oBB, blue) {
-    const lowestHash = hashKey(pBB, oBB);
-    if (hashMap.has(lowestHash)) {
-        return hashMap.get(lowestHash);
+    const hash = hashKey(pBB, oBB);
+    if (hashMap.has(hash)) {
+        return hashMap.get(hash);
     }
+
     const score = analysePosition(pBB, oBB, blue);
-    hashMap.set(lowestHash, score);
+    hashMap.set(hash, score);
     return score;
 }
 
 function analyseBoard() {
     let i, j, score, s;
     let best = -100;
-    const playerMoveChar = blueToMove ? 'O' : 'X';
-    const oppMoveChar = blueToMove ? 'X' : 'O';
-    const playerPotChar = blueToMove ? 'o' : 'x';
-    const oppPotChar = blueToMove ? 'x' : 'o';
+    const playerMoveChar = blueToMove ? "O" : "X";
+    const oppMoveChar = blueToMove ? "X" : "O";
+    const playerPotChar = blueToMove ? "o" : "x";
+    const oppPotChar = blueToMove ? "x" : "o";
 
     const playerValids = validMoves(playerBB, oppBB);
     const oppValids = validMoves(oppBB, playerBB);
@@ -63,9 +69,13 @@ function analyseBoard() {
     for (i = 0; i < numSquares; i += WIDTH) {
         s = "";
         for (j = i; j < i + WIDTH; j++) {
-            if (getBit(playerBB, j)) { s += playerMoveChar; }
-            else if (getBit(oppBB, j)) { s += oppMoveChar; }
-            else { s += "."; }
+            if (getBit(playerBB, j)) {
+                s += playerMoveChar;
+            } else if (getBit(oppBB, j)) {
+                s += oppMoveChar;
+            } else {
+                s += ".";
+            }
         }
 
         s += "\t";
@@ -89,7 +99,7 @@ function analyseBoard() {
                 }
                 s += score.toString().padStart(3, " ");
             } else {
-                s += "  ."
+                s += "  .";
             }
         }
         console.log(s);
@@ -118,7 +128,7 @@ function undoMove(m) {
 function bitCount(n) {
     n = n - ((n >> 1) & 0x55555555);
     n = (n & 0x33333333) + ((n >> 2) & 0x33333333);
-    return ((n + (n >> 4) & 0xF0F0F0F) * 0x1010101) >> 24;
+    return (((n + (n >> 4)) & 0xf0f0f0f) * 0x1010101) >> 24;
 }
 
 function getBit(bb, bitPosition) {
@@ -133,40 +143,52 @@ function clearBit(bb, bitPosition) {
     return bb & ~(1 << bitPosition);
 }
 
-function validMovesGen(width, height) {
-    const maskAll = (1 << width * height) - 1;
-    let maskExTop = maskAll,
-        maskExBottom = maskAll,
-        maskExLeft = maskAll,
-        maskExRight = maskAll;
-    for (let i = 0; i < width; i++) {
-        maskExTop = clearBit(maskExTop, i);
-        maskExBottom = clearBit(maskExBottom, width * (height - 1) + i);
+function linearBitSequence(bitStart, bitDifference, numTerms) {
+    console.assert(numTerms >= 1, `numTerms: ${numTerms} is less than one`);
+    let b = 1;
+    for (let i = 1; i < numTerms; i++) {
+        b = 1 + (b << bitDifference);
     }
-    for (let i = 0; i < width * height; i += width) {
-        maskExLeft = clearBit(maskExLeft, i);
-        maskExRight = clearBit(maskExRight, i + width - 1);
+    return b << bitStart;
+}
+
+function validMovesGen(width, height) {
+    const maskAll = linearBitSequence(0, 1, width * height);
+    const maskExLeft = maskAll & ~linearBitSequence(0, width, height);
+    const maskExRight = maskAll & ~linearBitSequence(width - 1, width, height);
+    const maskExTop = maskAll & ~linearBitSequence(0, 1, width);
+    const maskExBottom = maskAll & ~linearBitSequence(width * (height - 1), 1, width);
+
+    function validMoves(pBB, oBB) {
+        const forbiddenMoves = pBB | oBB |
+            ((oBB & maskExBottom) << width) |
+            ((oBB & maskExTop) >> width) |
+            ((oBB & maskExRight) << 1) |
+            ((oBB & maskExLeft) >> 1);
+        return maskAll & ~forbiddenMoves;
     }
 
-    return function (pBB, oBB) {
-        return maskAll & ~(pBB | oBB |
-            (oBB & maskExBottom) << width |
-            (oBB & maskExTop) >> width |
-            (oBB & maskExRight) << 1 |
-            (oBB & maskExLeft) >> 1);
-    };
+    return validMoves;
 }
 
 function hashKeyGen(width, height) {
     const encodeMultiplier = 2 ** (width * height);
-    const numSymmetries = (width === height) ? 8 : 4;
-    const [rowMask, rowShifts, rowStartMask, rowLastIndex] = rowReflectStates(width, height);
-    const [lineMask, lineShifts, lineStartMask, lineLastIndex] = lineReflectStates(width, height);
+    const numSymmetries = width === height ? 8 : 4;
+
+    const [rowMask, rowShifts, rowStartMask, rowLastIndex] =
+        rowReflectStates(width, height);
+
+    const [lineMask, lineShifts, lineStartMask, lineLastIndex] =
+        (width === height) ?
+            diagReflectStates(width) :
+            colReflectStates(width, height);
 
     function rowReflect(bb) {
         let ans = bb & rowStartMask;
         for (const [i, shift] of rowShifts.entries()) {
-            ans |= (bb & rowMask[i]) << shift | (bb & rowMask[rowLastIndex - i]) >> shift;
+            ans |=
+                ((bb & rowMask[i]) << shift) |
+                ((bb & rowMask[rowLastIndex - i]) >> shift);
         }
         return ans;
     }
@@ -174,7 +196,9 @@ function hashKeyGen(width, height) {
     function lineReflect(bb) {
         let ans = bb & lineStartMask;
         for (const [i, shift] of lineShifts.entries()) {
-            ans |= (bb & lineMask[i]) << shift | (bb & lineMask[lineLastIndex - i]) >> shift;
+            ans |=
+                ((bb & lineMask[i]) << shift) |
+                ((bb & lineMask[lineLastIndex - i]) >> shift);
         }
         return ans;
     }
@@ -183,45 +207,28 @@ function hashKeyGen(width, height) {
         return bb1 * encodeMultiplier + bb2;
     }
 
-    return function (pBB, oBB) {
+    function hashKey(pBB, oBB) {
         let lowestHash = encode(pBB, oBB);
         for (let i = 0; i < numSymmetries - 1; i++) {
-            const op = (i % 2 === 0) ? rowReflect : lineReflect;
+            const op = i % 2 === 0 ? rowReflect : lineReflect;
             pBB = op(pBB);
             oBB = op(oBB);
             const newHash = encode(pBB, oBB);
-            lowestHash = (newHash < lowestHash) ? newHash : lowestHash;
+            lowestHash = newHash < lowestHash ? newHash : lowestHash;
         }
         return lowestHash;
-    };
-}
-
-
-function lineReflectStates(width, height) {
-    if (width === height) {
-        return diagReflectStates(width);
-    } else {
-        return colReflectStates(width, height);
     }
+
+    return hashKey;
 }
 
 function diagReflectStates(width) {
     const lineMask = Array();
-    for (let d = 0; d < width; d++) {
-        let newDiag = 0;
-        const numElements = d + 1;
-        for (let i = 0; i < numElements; i++) {
-            newDiag = setBit(newDiag, d + i * (width - 1));
-        }
-        lineMask.push(newDiag);
+    for (let c = 0; c < width; c++) {
+        lineMask.push(linearBitSequence(c, width - 1, c + 1));
     }
-    for (let d = 1; d < width; d++) {
-        let newDiag = 0;
-        const numElements = width - d;
-        for (let i = 0; i < numElements; i++) {
-            newDiag = setBit(newDiag, width - 1 + width * d + i * (width - 1));
-        }
-        lineMask.push(newDiag);
+    for (let r = 1; r < width; r++) {
+        lineMask.push(linearBitSequence(width * (r + 1) - 1, width - 1, width - r));
     }
 
     const lineShifts = Array();
@@ -230,16 +237,14 @@ function diagReflectStates(width) {
     }
 
     const diagStartMask = lineMask[width - 1];
-    const diagLastIndex = lineMask.length - 1
-    return [lineMask, lineShifts, diagStartMask, diagLastIndex]
+    const diagLastIndex = lineMask.length - 1;
+    return [lineMask, lineShifts, diagStartMask, diagLastIndex];
 }
 
 function colReflectStates(width, height) {
-    const colMask = Array(width).fill(0);
+    const colMask = Array();
     for (let c = 0; c < width; c++) {
-        for (let r = 0; r < height; r++) {
-            colMask[c] += 1 << (c + r * width);
-        }
+        colMask.push(linearBitSequence(c, width, height));
     }
 
     const colShifts = Array();
@@ -249,13 +254,13 @@ function colReflectStates(width, height) {
 
     const colStartMask = (width % 2 !== 0) ? colMask[(width - 1) / 2] : 0;
     const colLastIndex = colMask.length - 1;
-    return [colMask, colShifts, colStartMask, colLastIndex]
+    return [colMask, colShifts, colStartMask, colLastIndex];
 }
 
 function rowReflectStates(width, height) {
     const rowMask = Array(height).fill(0);
     for (let r = 0; r < height; r++) {
-        rowMask[r] = (1 << (r + 1) * width) - (1 << r * width);
+        rowMask[r] = linearBitSequence(r * width, 1, width);
     }
 
     const rowShifts = Array();
@@ -265,15 +270,14 @@ function rowReflectStates(width, height) {
 
     const rowStartMask = (height % 2 !== 0) ? rowMask[(height - 1) / 2] : 0;
     const rowLastIndex = rowMask.length - 1;
-    return [rowMask, rowShifts, rowStartMask, rowLastIndex]
+    return [rowMask, rowShifts, rowStartMask, rowLastIndex];
 }
-
 
 function mapSize(oMap) {
     function replacer(key, value) {
         if (value instanceof Map) {
             return {
-                dataType: 'Map',
+                dataType: "Map",
                 value: [...value],
             };
         } else {
